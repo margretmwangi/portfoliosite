55428950a86288a94f6dde36474fe186c9cd48f2
margretmwangi <margaret13mwangi@gmail.com>
2020-06-22 19:12:51 +0300

    Updates

---
+++ precache-manifest.32f464bbb382785eb4edc968644ce808.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "786274b042d11de83992def3979f23f0",
+    "url": "/portfoliosite/index.html"
+  },
+  {
+    "revision": "0be53e5af352fdcfe891",
+    "url": "/portfoliosite/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "e9d7f04b7739109bb6ae",
+    "url": "/portfoliosite/static/css/main.167b6fb5.chunk.css"
+  },
+  {
+    "revision": "0be53e5af352fdcfe891",
+    "url": "/portfoliosite/static/js/2.1ec91bc2.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfoliosite/static/js/2.1ec91bc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9d7f04b7739109bb6ae",
+    "url": "/portfoliosite/static/js/main.2f8b2bad.chunk.js"
+  },
+  {
+    "revision": "9bc02d9629cf4d3ac86f",
+    "url": "/portfoliosite/static/js/runtime-main.732b97db.js"
+  }
+]);
